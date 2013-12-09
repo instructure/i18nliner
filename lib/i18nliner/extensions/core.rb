51745c3ff72a94144cfd844ab1222e5d9af1bde8
Jon Jensen <jenseng@gmail.com>
2013-12-08 21:48:07 -0700

    wrapper application hooks
    apply wrappers when translating a string. accepts both array and hash
    wrappers. html-escape the string prior to wrapping (since it is assumed
    wrappers will have html content)

--- lib/i18nliner/extensions/core.rb
+++ lib/i18nliner/extensions/core.rb
@@ -1,5 +1,6 @@
 require 'i18nliner/scope'
 require 'i18nliner/call_helpers'
+require 'active_support/core_ext/string/output_safety'
 
 module I18nliner
   module Extensions
@@ -8,10 +9,34 @@ module I18nliner
 
       def translate(*args)
         key, default, options = *infer_arguments(Scope.root, args)
+        wrappers = options.delete(:wrappers)
         options ||= {}
-        super key, options.merge(:default => default)
+        result = super(key, options.merge(:default => default))
+        if wrappers
+          result = apply_wrappers(result, wrappers)
+        end
+        result
       end
       alias :t :translate
+
+     private
+
+      def apply_wrappers(string, wrappers)
+        string = string.html_safe? ? string.dup : ERB::Util.h(string)
+        if wrappers.is_a?(Array)
+          wrappers = Hash[wrappers.each_with_index.map{ |w, i| ['*' * (1 + i), w]}]
+        end
+        wrappers.sort_by{ |k, v| -k.length }.each do |k, v|
+          pattern = pattern_for(k)
+          string.gsub!(pattern, v)
+        end
+        string
+      end
+
+      def pattern_for(key)
+        escaped_key = Regexp.escape(key)
+        /#{escaped_key}(.*?)#{escaped_key}/
+      end
     end
   end
 end
--- spec/extensions/core_spec.rb
+++ spec/extensions/core_spec.rb
@@ -1,17 +1,36 @@
 require 'i18nliner/extensions/core'
 
 describe I18nliner::Extensions::Core do
-  it "should should normalize the arguments passed into the original translate" do
-    i18n = Module.new do
+  let(:i18n) do
+    Module.new do
       extend(Module.new do
         def translate(*args)
           spy(*args)
         end
+
+        def interpolate(string, options)
+          string % options
+        end
       end)
       extend I18nliner::Extensions::Core
     end
+  end
+
+  it "should should normalize the arguments passed into the original translate" do
     expect(i18n).to receive(:spy).with("hello_name_84ff273f", :default => "Hello %{name}", :name => "bob")
     i18n.translate("Hello %{name}", :name => "bob")
+  end
+
+  it "should apply wrappers" do
+    i18n.should_receive(:spy) { |*args| args[1][:default] }
+    i18n.translate("Hello *bob*. Click **here**", :wrappers => ['<b>\1</b>', '<a href="/">\1</a>']).
+      should == "Hello <b>bob</b>. Click <a href=\"/\">here</a>"
+  end
+
+  it "should html-escape the default when applying wrappers" do
+    i18n.should_receive(:spy) { |*args| args[1][:default] }
+    i18n.translate("*bacon* > narwhals", :wrappers => ['<b>\1</b>']).
+      should == "<b>bacon</b> &gt; narwhals"
   end
 end
 
