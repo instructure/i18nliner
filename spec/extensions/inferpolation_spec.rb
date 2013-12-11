b430a6b6b92ecf8d4be0f1adbf9a7896a616712f
Jon Jensen <jenseng@gmail.com>
2013-12-10 22:46:52 -0700

    slightly better spec for interpolation value inference

--- spec/extensions/inferpolation_spec.rb
+++ spec/extensions/inferpolation_spec.rb
@@ -2,36 +2,39 @@ require 'i18nliner/extensions/inferpolation'
 require 'ostruct'
 
 describe I18nliner::Extensions::Inferpolation do
-  
+
   let(:foo) do
-    foo = OpenStruct.new(:bar => OpenStruct.new(:baz => "lol"))
+    foo = OpenStruct.new(:bar => OpenStruct.new(:baz => "lol"), :bar2 => "foo")
     foo.instance_variable_set(:@bar, foo.bar)
+    foo.instance_variable_set(:@bar2, "foo")
     foo.extend I18nliner::Extensions::Inferpolation
     foo
   end
 
-  it "should inferpolate valid instance method chains" do
-    options = {:default => "hello %{bar.baz}"}
+  it "should inferpolate valid instance methods and chains" do
+    options = {:default => "hello %{bar.baz} %{bar2}"}
     foo.inferpolate(options).should == {
-      :default => "hello %{bar_baz}",
-      :bar_baz => foo.bar.baz
+      :default => "hello %{bar_baz} %{bar2}",
+      :bar_baz => foo.bar.baz,
+      :bar2 => foo.bar2
     }
   end
 
-  it "should inferpolate valid instance variable chains" do
-    options = {:default => "hello %{@bar.baz}"}
+  it "should inferpolate valid instance variables and chains" do
+    options = {:default => "hello %{@bar.baz} %{@bar2}"}
     foo.inferpolate(options).should == {
-      :default => "hello %{bar_baz}",
-      :bar_baz => foo.bar.baz
+      :default => "hello %{bar_baz} %{bar2}",
+      :bar_baz => foo.bar.baz,
+      :bar2 => foo.bar2
     }
   end
 
-  it "should not inferpolate invalid instance method chains" do
+  it "should not inferpolate invalid instance methods and chains" do
     options = {:default => "hello %{lol} %{bar.baz.lol}"}
     foo.inferpolate(options).should == options
   end
 
-  it "should not inferpolate invalid instance variable chains" do
+  it "should not inferpolate invalid instance variables and chains" do
     options = {:default => "hello %{@lol} %{@bar.baz.lol}"}
     foo.inferpolate(options).should == options
   end
