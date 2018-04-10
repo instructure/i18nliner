a4792b9cd4d4e26750ae6dbad84743ad0666a2c7
Cody Cutrer <cody@instructure.com>
2018-04-10 14:39:08 -0600

    fix bad reference
    ERB handlers got namespaced in Rails

--- lib/i18nliner/erubi.rb
+++ lib/i18nliner/erubi.rb
@@ -2,7 +2,7 @@ require 'action_view/template'
 require 'i18nliner/pre_processors/erb_pre_processor'
 
 module I18nliner
-  class Erubi < ::ActionView::Template::Handlers::Erubi
+  class Erubi < ::ActionView::Template::Handlers::ERB::Erubi
     def initialize(source, options = {})
       source = I18nliner::PreProcessors::ErbPreProcessor.new(source).result
       super(source, options)
